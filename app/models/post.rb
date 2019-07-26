213fc680f1a468c7d650dcd2eb16f6438711120e
Ubuntu <ubuntu@ip-172-31-32-88.ap-northeast-1.compute.internal>
2019-07-26 10:50:34 +0000

    dependent 추가

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
     has_many :likes, dependent: :destroy
     has_many :users, through: :likes
     
-    has_many :comments
+    has_many :comments, dependent: :destroy
 end
